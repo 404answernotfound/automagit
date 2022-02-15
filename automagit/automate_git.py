02d1370de599f7c1b528678909a31da746af2ff3
404answernotfound <404answernotfound.public@gmail.com>
2022-02-15 11:50:57 +0100

    automagit is uploading new log file 15022022

--- automagit/automate_git.py
+++ automagit/automate_git.py
@@ -7,6 +7,7 @@ import logging
 import sys
 import requests
 import os
+from yaml import load, dump
 
 from datetime import date
 
@@ -16,15 +17,13 @@ __license__ = "MIT"
 
 _logger = logging.getLogger(__name__)
 today = date.today().strftime("%d%m%Y")
+data = None
 
 
 # ---- CLI ----
 # The functions defined in this section are wrappers around the main Python
 # API allowing them to be called directly from the terminal as a CLI
 # executable/script.
-def project_check():
-    """Returns the answer"""
-    print('42')
 
 def parse_args(args):
     """Parse command line parameters
@@ -39,36 +38,19 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description="Automated Gittron Automata, AGA")
     parser.add_argument(dest="function", help="function to run", type=str, metavar="STRING")
     parser.add_argument('--param', dest="parameter", help="function parameters", type=str, metavar="STRING")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO,
-    )
-    parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG,
-    )
     return parser.parse_args(args)
 
-
-def setup_logging(loglevel):
-    """Setup basic logging
-
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
-    )
     
+def project_check():
+    """Returns the answer"""
+    print('42')
+
+def check_config():
+    with open('config/automata.yaml') as config:
+        data = load(config, Loader=yaml.FullLoader)
+        print(data)
+    print('42')
+
 def search_user(username):
     print(os.getcwd(), os.listdir())
     response = requests.get("https://api.github.com/users/{}/followers".format(username))
@@ -103,11 +85,11 @@ def main(args):
           (for example  ``["--verbose", "42"]``).
     """
     args = parse_args(args)
-    setup_logging(args.loglevel)
-    _logger.debug("Starting crazy automata...")
     print(args)
     if(args.function == 'project_check'):
         project_check()
+    if(args.function == 'check_config'):
+        check_config()
     if(args.function == 'search_user'):
         search_user(args.parameter)
     if(args.function == 'commit_with_message'):
